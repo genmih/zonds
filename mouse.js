9b0625b2eb18fa3e325207f73e2d19cbc9887e39
Henry <orsha99@yandex.ru>
2023-03-10 14:33:24 +0300

    first commit

---
+++ mouse.js
@@ -0,0 +1,8 @@
+
+  window.addEventListener("load", function(event) {
+    alert(" Устье \t \t  Задровье \t Заболотье \t Новаторов \n\n Болбасово \t Ореховск \t Зубово \t  \t  Высокое \n\n Межево \t Барань \t\t Крапивно \t Лисуны");
+
+  });
+
+event();
+
